c8b1f0afd0e29a66cc91c7ff46cad48251a38f80
groshiniprasad <roshiniprasad10@gmail.com>
2024-09-28 23:54:29 +0300

    added migrations for user table

--- Makefile
+++ Makefile
@@ -3,3 +3,18 @@ build:
 
 run: build
 	@./bin/uploady
+
+# Use 'MIGRATION_NAME' to pass the name of the migration you want to create
+migration:
+	@if [ -z "$(MIGRATION_NAME)" ]; then \
+		echo "Error: Please provide a migration name using MIGRATION_NAME=<name>"; \
+		exit 1; \
+	else \
+		migrate create -ext sql -dir cmd/migrate/migrations "$(MIGRATION_NAME)"; \
+	fi
+
+migrate-up:
+	@go run cmd/migrate/main.go up
+
+migrate-down:
+	@go run cmd/migrate/main.go down
--- configs/envs.go
+++ configs/envs.go
@@ -4,34 +4,41 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/lpernett/godotenv"
 )
 
 type Config struct {
-	Port       string
-	DBUser     string
-	DBPassword string
-	DBAddress  string
-	DBName     string
+	Port                   string
+	DBUser                 string
+	DBPassword             string
+	DBAddress              string
+	DBName                 string
+	JWTSecret              string
+	JWTExpirationInSeconds int64
 }
 
 var Envs = initConfig()
 
 func initConfig() Config {
-	// Load the .env file and handle the error if it fails
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Warning: .env file not found, using default environment variables")
+	// Load the .env file only if not in production
+	if os.Getenv("GO_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Warning: .env file not found, using default environment variables")
+		}
 	}
 
 	// Return the configuration struct with environment variables or default values
 	return Config{
-		Port:       getEnv("PORT", "8080"),
-		DBUser:     getEnv("DB_USER", "root"),
-		DBPassword: getEnv("DB_PASSWORD", "mypassword"),
-		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
-		DBName:     getEnv("DB_NAME", "uploady"),
+		Port:                   getEnv("PORT", "8080"),
+		DBUser:                 getEnv("DB_USER", "root"),
+		DBPassword:             getEnv("DB_PASSWORD", "mypassword"),
+		DBAddress:              fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
+		DBName:                 getEnv("DB_NAME", "uploady"),
+		JWTSecret:              getEnv("JWT_SECRET", "kya-secret-chahiye-aapko?"),
+		JWTExpirationInSeconds: getEnvAsInt("JWT_EXPIRATION_IN_SECONDS", 3600*24*7),
 	}
 }
 
@@ -39,6 +46,20 @@ func initConfig() Config {
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
+	}
+	return fallback
+}
+
+// getEnvAsInt retrieves an integer environment variable or falls back to the default value.
+// If the value cannot be converted to an int, it logs a warning and returns the fallback.
+func getEnvAsInt(key string, fallback int64) int64 {
+	if value, ok := os.LookupEnv(key); ok {
+		i, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			log.Printf("Warning: Environment variable %s is not a valid integer, using default value %d", key, fallback)
+			return fallback
+		}
+		return i
 	}
 	return fallback
 }
